3ec470a22634dd4884627c56923ae401cdd53700
Me <dyulario@mail.ru>
2022-08-28 12:18:56 +0300

    2 задачи из 6го ДЗ по C#

---
+++ DZ06/Ex41/Program.cs
@@ -0,0 +1,13 @@
+﻿int count(int[] b)
+{
+    int result = 0; // кол-во чисел > 0   
+    for (int i = 0; i < b.Length; i = i + 1)
+        if (b[i] > 0) result = result + 1;
+    return result;
+}
+int m = new Random().Next(1, 6);
+Console.WriteLine($"Случайное число элементов m={m}, введи элементы");
+int[] a = new int[m];
+for (int i = 0; i < m; i = i + 1)
+    a[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество чисел > 0: {count(a)}");
---
+++ DZ06/Ex43/Program.cs
@@ -0,0 +1,16 @@
+﻿void point(int k1, int b1, int k2, int b2)
+{
+    double x = (double)(b2 - b1) / (k1 - k2);
+    double y = (double)(b2*k1 - b1*k2) / (k1 - k2);
+    if (k1 == k2) Console.WriteLine("Прямые не пересекаются");
+    else Console.WriteLine($"Координаты точки пересечения ({Math.Round(x, 2)},{Math.Round(y, 2)})");
+}
+Console.WriteLine("Введи k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+point(k1, b1, k2, b2);
